55445a4b825cb9c1c149b3839288246c00bd35d1
Lily <lilyneedsanemail@gmail.com>
2021-09-21 23:05:28 -0400

    Make sure message is empty for component_move

--- commands/command.ts
+++ commands/command.ts
@@ -1,9 +1,9 @@
 import move from "./move.ts";
-import component_move from './component_move.ts'
+import component_move from "./component_move.ts";
 
 const commands: Record<string, Function> = {
   move,
-  component_move
+  component_move,
 };
 
 export default commands;
--- commands/component_move.ts
+++ commands/component_move.ts
@@ -33,6 +33,8 @@ export default async (
   const response: APIInteractionResponseChannelMessageWithSource = {
     type: 7,
     data: {
+      content: "",
+      components: [],
       embeds: [embed],
     },
   };
